c5456a7d61394bed5736f4309d250d2eb1e097b0
EErickson <avilasonson@gmail.com>
2023-07-30 19:16:08 +0800

    modify routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\SorterController;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::post('/login' , [LoginController::class,'login']);
@@ -12,4 +13,5 @@ Route::post('/register/users', [UserController::class, 'register']);
 Route::group(['middleware' => ['auth:sanctum']], function(){
     Route::get('/user', [UserController::class, 'user']);
     Route::post('/add/customer', [CustomerController::class, 'addCustomer']);
+    Route::post('/add/sorter', [SorterController::class, 'addSorter']);
 });
