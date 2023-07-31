8b511b22c709ef0ba3d6f5bac8163fd31ef92bbe
EErickson <avilasonson@gmail.com>
2023-07-31 23:04:30 +0800

    fetch all company routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,4 +15,11 @@ class UserController extends Controller
             'user' => $user
         ]);
     }
+
+    public function getCompaniesInfo(){
+        $companies = User::with('customer')->get();
+        return response() -> json([
+            'companies' => $companies
+        ]);
+    }
 }
