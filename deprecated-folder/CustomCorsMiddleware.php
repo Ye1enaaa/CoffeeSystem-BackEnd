c6ce7c18c64a23d2d205074939c4091ca6384d98
EErickson <avilasonson@gmail.com>
2023-08-10 01:26:35 +0800

    :truck: change dir

---
+++ deprecated-folder/CustomCorsMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomCorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Origin', 'http://192.168.254.111:8000');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        
+        return $response;
+    }
+}
