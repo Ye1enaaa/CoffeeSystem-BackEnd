bf222faff5c2f4715202794802d6781a217d7a67
EErickson <avilasonson@gmail.com>
2023-09-02 18:43:40 +0800

    :hammer: refactor code upon registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,27 +3,39 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class RegisterController extends Controller
 {
     public function register(Request $request)
     {
-        $validate = $request->validate([
-            'name' => 'required|string|max:70',
-            'email' => 'required|string|unique:users,email',
-            'password' => 'required|string|min:6',
-        ]);
-
-        $userCreds = User::create([
-            'name' => $validate['name'],
-            'email' => $validate['email'],
-            'password' => bcrypt($validate['password'])
-        ]);
-        
-        return response() -> json([
-            'user' => $userCreds,
-            'status' => 'OK'
-        ], 200);
+        try {
+            $validate = $request->validate([
+                'name' => 'required|string|max:70',
+                'email' => 'required|string|unique:users,email',
+                'password' => 'required|string|min:6',
+            ]);
+    
+            $userCreds = User::create([
+                'name' => $validate['name'],
+                'email' => $validate['email'],
+                'password' => bcrypt($validate['password'])
+            ]);
+            
+            return response() -> json([
+                'user' => $userCreds,
+                'status' => 'OK'
+            ], 200);
+        } catch (ValidationException $e){
+            return response() -> json([
+                'error' => $e->validator->errors()->toArray(),
+                'status' => 'Validation error'
+            ], 422);
+        } catch(\Exception $e){
+            return response() -> json([
+                'error' => 'Something Went Wrong'
+            ], 500);
+        }
     }
 }
