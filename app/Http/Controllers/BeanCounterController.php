5aa68467c18707b692dd2dd1fc88fc8810134e1a
EErickson <avilasonson@gmail.com>
2023-10-05 16:15:47 +0800

    :sparkles: add bean count cntroller

--- app/Http/Controllers/BeanCounterController.php
+++ app/Http/Controllers/BeanCounterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\BeanCount;
+use App\Models\BeanData;
 class BeanCounterController extends Controller
 {
     public function fetchBeanCount(){
@@ -12,5 +13,22 @@ class BeanCounterController extends Controller
         return response()->json([
             'beans' => $bean
         ]);
+    }
+
+    public function postBeanCount(Request $request){
+        $id = $request->input('id');
+        $good = $request->input('good');
+        $bad = $request->input('bad');
+
+        $beanData = BeanData::create([
+            //'id' => $id,
+            'good' => $good,
+            'bad' => $bad
+        ]);
+
+        return response()->json([
+            'message' => 'OK',
+            'data' => $beanData
+        ], 200);
     }
 }
