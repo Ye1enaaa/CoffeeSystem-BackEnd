0747db3706183820d1c41272c99e0c195c4113a5
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-10-24 11:47:40 +0800

    update feedback controller

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -10,18 +10,34 @@ class FeedbackController extends Controller
     public function postFeedback(Request $request)
     {
         // Validate the incoming data
-        $validatedData = $request->validate([
-            'full_name' => 'required|string',
-            'companyName' => 'required|string',
-            'phoneNum' => 'required|string',
-            'coffee_bean_ai_sorter' => 'boolean',
-            'website' => 'boolean',
-            'message' => 'required|string',
-            'email' => 'required|email|unique:feedback,email',
-        ]);
+        // $validatedData = $request->validate([
+        //     'full_name' => 'required|string',
+        //     'companyName' => 'required|string',
+        //     'phoneNum' => 'required|string',
+        //     'coffee_bean_ai_sorter' => 'boolean',
+        //     'website' => 'boolean',
+        //     'message' => 'required|string',
+        //     'email' => 'required|email|unique:feedback,email',
+        // ]);
+
+        $full_name = $request->input('full_name');
+        $companyName = $request->input('companyName');
+        $phoneNum = $request->input('phoneNum');
+        $coffee_bean_ai_sorter = $request->input('coffee_bean_ai_sorter');
+        $website = $request->input('website');
+        $message = $request->input('message');
+        $email = $request->input('email');
 
         // Create a new Feedback record
-        $feedback = Feedback::create($validatedData);
+        $feedback = Feedback::create([
+            'full_name' => $full_name,
+            'companyName' => $companyName,
+            'phoneNum' => $phoneNum,
+            'coffee_bean_ai_sorter' => $coffee_bean_ai_sorter,
+            'website' => $website,
+            'message' => $message,
+            'email' => $email
+        ]);
 
         // Optionally, you can return a response or redirect as needed
         return response()->json(['message' => 'Feedback submitted successfully'], 201);
