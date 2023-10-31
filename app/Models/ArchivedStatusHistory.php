0e47a6fe0fbf9434113549e6092eb0bb1dc780bc
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-10-31 18:41:25 +0800

    added archived status table

---
+++ app/Models/ArchivedStatusHistory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ArchivedStatusHistory extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'customer_id',
+        'user_id',
+        'customerName',
+        'sorterName',
+        'kiloOfBeans',
+        'status'
+    ];
+}
