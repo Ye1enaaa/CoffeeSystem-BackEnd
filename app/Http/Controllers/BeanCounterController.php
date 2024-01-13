fa05ddc6159768b977dd60b2ecb904fc7fca09d0
EErickson <avilasonson@gmail.com>
2024-01-13 20:41:13 +0800

    modify code structure on fetching data in dashboard

--- app/Http/Controllers/BeanCounterController.php
+++ app/Http/Controllers/BeanCounterController.php
@@ -10,54 +10,76 @@ use App\Models\Status;
 use App\Models\Customer;
 class BeanCounterController extends Controller
 {
+    // public function fetchBeanCount($machineID, $userId)
+    // {
+    //     // Get the latest BeanData for the specified machineID
+    //     $latestBeanData = BeanData::where('machineID', $machineID);
+    //     $latestBeanDataTwos = BeanDataTwo::where('machineID', $machineID);
+
+    //     // Get the latest overall BeanData
+    //     $latestOverallBeanData = $latestBeanData->latest()->first();
+
+    //     // Get the latest overall BeanDataTwo
+    //     $latestOverallBeanDataTWo = $latestBeanDataTwos->latest()->first();
+
+    //     // Get all BeanData records for the specified machineID
+    //     $allBeans = BeanData::where('machineID', $machineID)->get();
+
+    //     $finishedStatus = Status::where('user_id', $userId)
+    //                    ->where('status', 'Finished')
+    //                    ->get();
+
+    //     // Calculate total kilos of bad beans
+    //     $totalBeans = $finishedStatus->sum('kiloOfBeans');
+
+    //     // Convert kilo to grams
+    //     $intTotalBeans = intval($totalBeans * 1000);
+
+    //     //convert to grams to kilo
+    //     $totalBad = $latestOverallBeanData->bad + $latestOverallBeanDataTWo->bad;
+    //     $goodBeans = $intTotalBeans - $totalBad * 0.5;
+    //     $kiloBeans = $totalBad * 0.5 / 1000;
+    //     $kiloBeansTwo = $latestOverallBeanDataTWo->bad * 0.5 / 1000;
+    //     $good = $intTotalBeans - $kiloBeans;
+    //     $kiloBeans2 = $intTotalBeans - $kiloBeans;
+    //     $TotalkiloBeans = $kiloBeans + $kiloBeans2;
+
+    //     // Return the response
+    //     return response()->json([
+    //         'beans' => $latestOverallBeanData,
+    //         'total' => $kiloBeans2,
+    //         'TotalBadBeans' => $totalBad,
+    //         'GramsBadBeans' => $kiloBeans,
+    //         'kiloGoodBeans' => $good,
+    //         // 'kiloBeans2' => $kiloBeans2,
+    //         'allBeans' => $allBeans,
+    //         'goodbeans' => round($goodBeans),
+    //         'status' => $finishedStatus,
+    //         'total' => $totalBeans
+    //     ]);
+    // }
+
     public function fetchBeanCount($machineID, $userId)
     {
-        // Get the latest BeanData for the specified machineID
-        $latestBeanData = BeanData::where('machineID', $machineID);
-        $latestBeanDataTwos = BeanDataTwo::where('machineID', $machineID);
+        $statuses = Status::where('user_id', $userId)->get();
+        
+        $totalBadCount = 0;
+        $totalGoodCount = 0;
+        $totalKg = 0;
 
-        // Get the latest overall BeanData
-        $latestOverallBeanData = $latestBeanData->latest()->first();
-
-        // Get the latest overall BeanDataTwo
-        $latestOverallBeanDataTWo = $latestBeanDataTwos->latest()->first();
-
-        // Get all BeanData records for the specified machineID
-        $allBeans = BeanData::where('machineID', $machineID)->get();
-
-        $finishedStatus = Status::where('user_id', $userId)
-                       ->where('status', 'Finished')
-                       ->get();
-
-        // Calculate total kilos of bad beans
-        $totalBeans = $finishedStatus->sum('kiloOfBeans');
-
-        // Convert kilo to grams
-        $intTotalBeans = intval($totalBeans * 1000);
-
-        //convert to grams to kilo
-        $totalBad = $latestOverallBeanData->bad + $latestOverallBeanDataTWo->bad;
-        $goodBeans = $intTotalBeans - $totalBad * 0.5;
-        $kiloBeans = $totalBad * 0.5 / 1000;
-        $kiloBeansTwo = $latestOverallBeanDataTWo->bad * 0.5 / 1000;
-        $good = $intTotalBeans - $kiloBeans;
-        $kiloBeans2 = $intTotalBeans - $kiloBeans;
-        $TotalkiloBeans = $kiloBeans + $kiloBeans2;
-
-        // Return the response
+        foreach ($statuses as $status) {
+            $totalBadCount += $status->badCount;
+            $totalGoodCount += $status->goodCount;
+            $totalKg += $status->kiloOfBeans;
+        }
+    
         return response()->json([
-            'beans' => $latestOverallBeanData,
-            'total' => $kiloBeans2,
-            'TotalBadBeans' => $totalBad,
-            'GramsBadBeans' => $kiloBeans,
-            'kiloGoodBeans' => $good,
-            // 'kiloBeans2' => $kiloBeans2,
-            'allBeans' => $allBeans,
-            'goodbeans' => round($goodBeans),
-            'status' => $finishedStatus,
-            'total' => $totalBeans
+            'badBeans' => $totalBadCount,
+            'goodBeans' => $totalGoodCount,
+            'totalKg' => $totalKg
         ]);
     }
+    
 
     public function postBeanCount(Request $request){
         $machineID = $request->input('machineID');
